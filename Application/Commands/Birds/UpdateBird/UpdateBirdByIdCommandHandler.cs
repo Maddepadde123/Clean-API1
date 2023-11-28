e3d07c8e16f8409d25bced544e31d39d22b4433c
Maddepadde123 <114986735+Maddepadde123@users.noreply.github.com>
2023-11-28 09:48:33 +0100

    Finished errors in pull request

--- Application/Commands/Birds/UpdateBird/UpdateBirdByIdCommandHandler.cs
+++ Application/Commands/Birds/UpdateBird/UpdateBirdByIdCommandHandler.cs
@@ -15,10 +15,8 @@ namespace Application.Commands.Birds.UpdateBird
         public Task<Bird> Handle(UpdateBirdByIdCommand request, CancellationToken cancellationToken)
         {
             Bird birdToUpdate = _mockDatabase.Birds.FirstOrDefault(bird => bird.Id == request.Id)!;
-
             birdToUpdate.Name = request.UpdatedBird.Name; 
             birdToUpdate.CanFly = request.UpdatedBird.CanFly; 
-
             return Task.FromResult(birdToUpdate);
         }
     }
